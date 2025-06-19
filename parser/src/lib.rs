dfc6c171609f2852a0bacb44b7deb5d575026f21
SpeakBoy <nevinspeakman@outlook.com>
2025-06-19 19:43:04 -0400

    Remove comments, fix decimal_to_binary

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -3,7 +3,6 @@ use std::fs;
 
 pub struct Parser {
     file_path: String,
-    input_string: String,
     symbol_table: HashMap<String, String>,
 }
 
@@ -11,7 +10,6 @@ impl Parser {
     pub fn new(file_path: String) -> Parser {
         Parser {
             file_path,
-            input_string: String::new(),
             symbol_table: HashMap::new(),
         }
     }
@@ -20,14 +18,13 @@ impl Parser {
         let contents =
             fs::read_to_string(&self.file_path).expect("Should have been able to read the file");
 
-        self.input_string.push_str(&contents);
-
         let lines = contents.lines();
 
         for line in lines {
             let blank = line.trim().is_empty();
+            let comment = line.contains('/');
             let mut instruction = String::new();
-            if !blank {
+            if !blank && !comment {
                 let first_char = line.chars().nth(0).expect("Out of range");
                 if first_char == '@' {
                     instruction.push_str(&(self.decode_a_instruction(line)));
@@ -51,9 +48,9 @@ impl Parser {
         let mut output = String::new();
         loop {
             if num % 2 == 1 {
-                output.push('1');
+                output = String::from("1") + &output;
             } else {
-                output.push('0');
+                output = String::from("0") + &output;
             }
             num /= 2;
             if num == 0 {
@@ -65,5 +62,11 @@ impl Parser {
         let zero_padding = "0".repeat(diff);
         let output = zero_padding + &output;
         output
+    }
+
+    fn decode_c_instruction(&mut self, line: &str) -> String {
+        let mut first_digits = String::from("111");
+
+        first_digits
     }
 }
