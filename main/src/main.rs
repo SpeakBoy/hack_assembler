2f29916a68c6a806607cdf4f35dbb2bbcf73d9b9
SpeakBoy <nevinspeakman@outlook.com>
2025-06-19 17:25:54 -0400

    Read asm files line-by-line

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,17 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path = &args[1];
+
+    println!("{file_path}");
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let lines = contents.lines();
+
+    for line in lines {
+        println!("{line}");
+    }
 }
